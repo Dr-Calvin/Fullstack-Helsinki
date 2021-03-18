b81ad7800cc330806f65abf44465d8235df41b77
Mitchel Follett <lonadois@gmail.com>
2021-03-18 01:45:24 +0000

    Part1.4 completed

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,52 +5,59 @@ const Header = (props) => {
 };
 const Part = (props) => {
   return (
-    <p>
-      {props.part} {props.ex}
-    </p>
+    (
+      <p>
+        {props.data.name} {props.data.exercises}
+      </p>
+    )
   );
 };
 const Content = (props) => {
   return (
-    console.log(props),
     <div>
-      <Part part={props.p1[0]} ex={props.p1[1]} />
-      <Part part={props.p2[0]} ex={props.p2[1]} />
-      <Part part={props.p3[0]} ex={props.p3[1]} />
+      <Part data={props.parts[0]} />
+      <Part data={props.parts[1]} />
+      <Part data={props.parts[2]} />
     </div>
   );
 };
 const Total = (props) => {
-  return <p>Number of exercises {props.sum}</p>;
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </p>
+  );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        p1={[part1.name, part1.exercises]}
-        p2={[part2.name, part2.exercises]}
-        p3={[part3.name, part3.exercises]}
-      />
-      <Total sum={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
 
 export default App;
+
+// <Total sum={part1.exercises + part2.exercises + part3.exercises} />;
